a684b4c34ecc4bb17cddf3d21d9092a0ccf79a1e
eselse <ilikewebdevelopment@gmail.com>
2022-02-22 01:21:33 -0500

    add source code for first three topics

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ print(type(alarm))
 print(type(offline))
 
 # *-*-*-*-*--------------- Strings **************************
+# Immutable
 movie_name = "The good, bad and ugly"
 print(movie_name)
 print(type(movie_name))
@@ -94,6 +95,7 @@ user_info = {"user_name": "awesome50", "user_id": 56}
 # print(type(user_info))
 
 # *-*-*-*-*--------------- Tuple *******************************
+# Immutable
 # ordered
 mixed_tuple = (1, 2, 3.2, 5.4, True, "Muslim Helalee", [1, 2, 3])
 # print(mixed_tuple)
@@ -105,3 +107,62 @@ mixed_tuple = (1, 2, 3.2, 5.4, True, "Muslim Helalee", [1, 2, 3])
 mixed_set = {1, 2, 3.2, 5.4, "Python", "Muslim Helalee", 101}
 print(mixed_set)
 print(type(mixed_set))
+
+# ------------------------------------------------------------
+# -----------------**## String Methods ##**-------------------
+
+# 1- len()
+address = "Afghanistan"
+# print(len(address))
+
+# 2- [] Notation
+# print(address[0])
+# print(address[7])
+# print(address[-1])
+# print(address[-4])
+
+# 3- [] Notation
+# print(address[0:4])
+# print(address[-7:-1])
+# print(address[0:])
+# print(address[:9])
+# print(address[:])
+
+# 4- Concatenation ->>>> Formatted Strings
+# country = "USA"
+# city = "NYC"
+# full_address = city + ", " + country
+# full_address = f"{city}, {country}"
+# print(full_address)
+
+# 5- upper()
+# print(address.upper())
+
+# 6- lower()
+# print(city.lower())
+
+# 7- title()
+# print("---------------", full_address.title(), "---------------")
+
+# 8- strip()
+job = "       Programmer   "
+# print(job)
+# print(job.strip())
+# print(job.lstrip())
+# print(job.rstrip())
+
+# 9- find()
+# print(address.find("n"))
+# print(address.find("i"))
+
+# 10- replace()
+# print(address.replace("f", "Z"))
+
+# 11- in operator
+# print("ne" in address)
+# print("ni" in address)
+
+# 12- not operator
+# print("ne" not in address)
+# print("ne" not in address)
+# print("ni" not in address)
